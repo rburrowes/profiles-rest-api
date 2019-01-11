3c6ce7756b244681826936c357ea015f7d50e0f1
raymond-burrowes <raymond.burrowes@maplink.global>
2019-01-11 16:50:23 +0100

    Added the viewset and the URL router.

--- src/profiles_project/profile_api/views.py
+++ src/profiles_project/profile_api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
 
 from . import serializers
-from rest_framework import status
 
 # Create your views here.
 
@@ -52,3 +53,17 @@ class HelloApiView(APIView):
         """Deletes an object that exists."""
 
         return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message."""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)'
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
